1516247097c8a280d72bb43f385b321457c830a5
jtardioli <joshua.tardioli@gmail.com>
2024-04-22 19:54:58 -0300

    fix imports

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract BoxV2 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+    constructor() {
+        _disableInitializers();
+        __UUPSUpgradeable_init();
+    }
 
     function setNumber(uint256 _number) external {}
 
@@ -13,4 +22,6 @@ contract BoxV2 {
     function version() external pure returns (uint256) {
         return 2;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
